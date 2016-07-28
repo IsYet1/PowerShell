51be962223694552fba758ae94c934d66153a54b
IsYet1 <Don@IsYet.com>
2016-07-28 09:02:31 -0500

    Added 1 clip button and function
    1st clipboard button added. Works.

--- Concepts/Buttons00.ps1
+++ Concepts/Buttons00.ps1
@@ -1,94 +1,120 @@
 ﻿#Generated Form Function
 function GenerateForm {
 
-#region Import the Assemblies
-[reflection.assembly]::loadwithpartialname("System.Windows.Forms") | Out-Null
-[reflection.assembly]::loadwithpartialname("System.Drawing") | Out-Null
-#endregion
+    #region Import the Assemblies
+        [reflection.assembly]::loadwithpartialname("System.Windows.Forms") | Out-Null
+        [reflection.assembly]::loadwithpartialname("System.Drawing") | Out-Null
+    #endregion
 
-#region Generated Form Objects
-$MainForum = New-Object System.Windows.Forms.Form
-$GetServices = New-Object System.Windows.Forms.Button
-$richTextBox1 = New-Object System.Windows.Forms.RichTextBox
-$InitialFormWindowState = New-Object System.Windows.Forms.FormWindowState
-#endregion Generated Form Objects
+    #region Generated Form Objects
+        $MainForum = New-Object System.Windows.Forms.Form
+        $GetServices = New-Object System.Windows.Forms.Button
+        $ClipBoard01 = New-Object System.Windows.Forms.Button
+        $richTextBox1 = New-Object System.Windows.Forms.RichTextBox
+        $InitialFormWindowState = New-Object System.Windows.Forms.FormWindowState
+    #endregion Generated Form Objects
 
-#----------------------------------------------
-#Generated Event Script Blocks
-#----------------------------------------------
-#Provide Custom Code for events specified in PrimalForms.
-
-
-$OnLoadForm_StateCorrection=
-{#Correct the initial state of the form to prevent the .Net maximized form issue
-    $MainForum.WindowState = $InitialFormWindowState
-}
-
-#----------------------------------------------
-#region Generated Form Code
-$System_Drawing_Size = New-Object System.Drawing.Size
-$System_Drawing_Size.Height = 262
-$System_Drawing_Size.Width = 461
-$MainForum.ClientSize = $System_Drawing_Size
-$MainForum.DataBindings.DefaultDataSourceUpdateMode = 0
-$MainForum.Name = "MainForum"
-$MainForum.Text = "Demo GUI"
+    #----------------------------------------------
+    #Generated Event Script Blocks
+    #----------------------------------------------
+    #Provide Custom Code for events specified in PrimalForms.
 
 
-$GetServices.DataBindings.DefaultDataSourceUpdateMode = 0
+    $OnLoadForm_StateCorrection=
+        {#Correct the initial state of the form to prevent the .Net maximized form issue
+            $MainForum.WindowState = $InitialFormWindowState
+        }
 
-$System_Drawing_Point = New-Object System.Drawing.Point
-$System_Drawing_Point.X = 13
-$System_Drawing_Point.Y = 26
-$GetServices.Location = $System_Drawing_Point
-$GetServices.Name = "GetServices"
-$System_Drawing_Size = New-Object System.Drawing.Size
-$System_Drawing_Size.Height = 23
-$System_Drawing_Size.Width = 75
-$GetServices.Size = $System_Drawing_Size
-$GetServices.TabIndex = 1
-$GetServices.Text = "GetServices"
-$GetServices.UseVisualStyleBackColor = $True
-$GetServices.add_Click({ GetServices($GetServices) })
-$richTextBox1.DataBindings.DefaultDataSourceUpdateMode = 0
-$System_Drawing_Point = New-Object System.Drawing.Point
-$System_Drawing_Point.X = 12
-$System_Drawing_Point.Y = 132
-$richTextBox1.Location = $System_Drawing_Point
-$richTextBox1.Name = "richTextBox1"
-$System_Drawing_Size = New-Object System.Drawing.Size
-$System_Drawing_Size.Height = 118
-$System_Drawing_Size.Width = 437
-$richTextBox1.Size = $System_Drawing_Size
-$richTextBox1.TabIndex = 0
-$richTextBox1.Text = "Results Out Put To This Box"
-
-$MainForum.Controls.Add($richTextBox1)
-$MainForum.Controls.Add($GetServices)
-#endregion
-function GetServices($object)
-{
- OutputText
-}
+    #----------------------------------------------
+    #region Generated Form Code
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 262
+        $System_Drawing_Size.Width = 461
+        $MainForum.ClientSize = $System_Drawing_Size
+        $MainForum.DataBindings.DefaultDataSourceUpdateMode = 0
+        $MainForum.Name = "MainForum"
+        $MainForum.Text = "Demo GUI"
 
 
-function OutputText($object)
-{
-$service = (Get-Service s*) 
- foreach ($_ in $service)
- {
- $name = $_.Name
- $status = $_.Status
- $richTextBox1.Text = $richTextBox1.Text + $name + "  "  +$status  + "`r"
-}
-}
+    $GetServices.DataBindings.DefaultDataSourceUpdateMode = 0
 
-#Save the initial state of the form
-$InitialFormWindowState = $MainForum.WindowState
-#Init the OnLoad event to correct the initial state of the form
-$MainForum.add_Load($OnLoadForm_StateCorrection)
-#Show the Form
-$MainForum.ShowDialog()| Out-Null
+        $System_Drawing_Point = New-Object System.Drawing.Point
+        $System_Drawing_Point.X = 13
+        $System_Drawing_Point.Y = 26
+
+    $GetServices.Location = $System_Drawing_Point
+    $GetServices.Name = "GetServices"
+
+    $System_Drawing_Size = New-Object System.Drawing.Size
+    $System_Drawing_Size.Height = 23
+    $System_Drawing_Size.Width = 105
+
+    #Clipboard button 1
+        $System_Drawing_Point.X = 13
+        $System_Drawing_Point.Y = 60
+
+        $ClipBoard01.Location = $System_Drawing_Point
+        $ClipBoard01.Name = "TagName"
+        $ClipBoard01.Text = "Tag Name"
+        $ClipBoard01.Size = $System_Drawing_Size
+        $ClipBoard01.add_Click({clipper "Tag Name Clipped"})
+
+    $GetServices.Size = $System_Drawing_Size
+    $GetServices.TabIndex = 1
+    $GetServices.Text = "Get Services"
+    $GetServices.UseVisualStyleBackColor = $True
+
+    $GetServices.add_Click({ GetServices($GetServices) })
+
+    $richTextBox1.DataBindings.DefaultDataSourceUpdateMode = 0
+    $System_Drawing_Point = New-Object System.Drawing.Point
+    $System_Drawing_Point.X = 12
+    $System_Drawing_Point.Y = 132
+    $richTextBox1.Location = $System_Drawing_Point
+    $richTextBox1.Name = "richTextBox1"
+
+    $System_Drawing_Size = New-Object System.Drawing.Size
+    $System_Drawing_Size.Height = 118
+    $System_Drawing_Size.Width = 437
+    $richTextBox1.Size = $System_Drawing_Size
+    $richTextBox1.TabIndex = 0
+    $richTextBox1.Text = "Results Out Put To This Box"
+
+    $MainForum.Controls.Add($richTextBox1)
+    $MainForum.Controls.Add($GetServices)
+    $MainForum.Controls.Add($ClipBoard01)
+
+    #endregion
+
+    function GetServices($object)
+    {
+        OutputText
+    }
+
+    function clipper($stringToClip)
+    {
+        Set-Clipboard $stringToClip
+        $richTextBox1.Text = $stringToClip
+    }
+
+
+    function OutputText($object)
+    {
+        $service = (Get-Service s*) 
+        foreach ($_ in $service)
+        {
+            $name = $_.Name
+            $status = $_.Status
+            $richTextBox1.Text = $richTextBox1.Text + $name + "  "  +$status  + "`r"
+        }
+    }
+
+    #Save the initial state of the form
+    $InitialFormWindowState = $MainForum.WindowState
+    #Init the OnLoad event to correct the initial state of the form
+    $MainForum.add_Load($OnLoadForm_StateCorrection)
+    #Show the Form
+    $MainForum.ShowDialog()| Out-Null
 
 } #End Function
 #Call the Function
