82c3803034b0674b60b330602b430d2c2fc01c4b
IsYet1 <Don@IsYet.com>
2016-07-29 18:29:55 -0500

    Adding the form
    Preparation for the buttons.

--- Utilities/PasteButtonsFromJson.ps1
+++ Utilities/PasteButtonsFromJson.ps1
@@ -1,12 +1,51 @@
-﻿$jsonFile = "AzureDemoClipboard.json"
+﻿    #region Read Json file
+        $jsonFile = "c:\git\powershell\utilities\AzureDemoClipboard.json"
 
-$buttonRows = ConvertFrom-Json -InputObject (Get-Content $jsonFile -Raw)
+        $buttonRows = ConvertFrom-Json -InputObject (Get-Content $jsonFile -Raw)
+    #endregion
+
+    #region Import the Assemblies
+        [reflection.assembly]::loadwithpartialname("System.Windows.Forms") | Out-Null
+        [reflection.assembly]::loadwithpartialname("System.Drawing") | Out-Null
+    #endregion
+
+    #region Rich Text Box
+        $richTextBox1 = New-Object System.Windows.Forms.RichTextBox
+        $richTextBox1.DataBindings.DefaultDataSourceUpdateMode = 0
+        $System_Drawing_Point = New-Object System.Drawing.Point
+        $System_Drawing_Point.X = 12
+        $System_Drawing_Point.Y = 400
+        $richTextBox1.Location = $System_Drawing_Point
+        $richTextBox1.Name = "richTextBox1"
+
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 50
+        $System_Drawing_Size.Width = 437
+        $richTextBox1.Size = $System_Drawing_Size
+        $richTextBox1.TabStop = $false #  TabIndex = 0
+
+        $MainForum.Controls.Add($richTextBox1)
+
+    #endregion
+
+    #region Main form
+        $MainForum = New-Object System.Windows.Forms.Form
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 800
+        $System_Drawing_Size.Width = 800
+        $MainForum.ClientSize = $System_Drawing_Size
+        $MainForum.DataBindings.DefaultDataSourceUpdateMode = 0
+        $MainForum.Name = "MainForum"
+        $MainForum.Text = "Clipboarder"
+
+        #Save the initial state of the form
+        $InitialFormWindowState = New-Object System.Windows.Forms.FormWindowState
+        $InitialFormWindowState = $MainForum.WindowState
+        #Init the OnLoad event to correct the initial state of the form
+        $MainForum.add_Load($OnLoadForm_StateCorrection)
+        #Show the Form
+        $MainForum.ShowDialog()| Out-Null
+    #endregion
 
 
 
-function readFile ()
-{
-    #return $buttonRows
-}
-
-#readFile
