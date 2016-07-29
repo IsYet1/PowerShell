6a31d7bc960b054e7f48ff9eb9040d4b93e7688b
IsYet1 <Don@IsYet.com>
2016-07-29 13:59:27 -0500

    Reading the json file
    Not processing correctly yet.

---
+++ Utilities/PasteButtonsFromJson.ps1
@@ -0,0 +1,12 @@
+﻿$buttonRows = @()
+$json = ""
+function readFile
+{
+    $json = Get-Content "AzureDemoClipboard.json" -Raw
+    write $json
+
+    $buttonRows = ConvertFrom-Json -InputObject $json
+    
+}
+
+readFile
