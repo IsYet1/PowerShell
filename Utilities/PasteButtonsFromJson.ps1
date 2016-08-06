7d913e8bf21965678386d1137220495829bb1885
IsYet1 <Don@IsYet.com>
2016-08-06 14:08:01 -0500

    Copied working code. Verified.

--- Utilities/PasteButtonsFromJson.ps1
+++ Utilities/PasteButtonsFromJson.ps1
@@ -20,7 +20,7 @@
     #region Read Json file
         $jsonFile = "c:\git\powershell\utilities\AzureDemoClipboard.json"
 
-        $buttonRows0 = ConvertFrom-Json -InputObject (Get-Content $jsonFile -Raw)
+        $buttonRows = ConvertFrom-Json -InputObject (Get-Content $jsonFile -Raw)
     #endregion
 
     #region Import the Assemblies
@@ -44,17 +44,32 @@
 
         $System_Drawing_Size = New-Object System.Drawing.Size
         $System_Drawing_Size.Height = 70
-        $System_Drawing_Size.Width = 200
+        $System_Drawing_Size.Width = 500
         $richTextBox1.Size = $System_Drawing_Size
         $richTextBox1.TabStop = $false #  TabIndex = 0
 
-        #$MainForum.Controls.Add($richTextBox1)
 
     #endregion
 
+    #region Main form
+        $MainForum = New-Object System.Windows.Forms.Form
+        $System_Drawing_Size = New-Object System.Drawing.Size
+        $System_Drawing_Size.Height = 800
+        $System_Drawing_Size.Width = 800
+        $MainForum.ClientSize = $System_Drawing_Size
+        $MainForum.DataBindings.DefaultDataSourceUpdateMode = 0
+        $MainForum.Name = "MainForum"
+        $MainForum.Text = "Clipboarder"
+
+        #Save the initial state of the form
+        $InitialFormWindowState = New-Object System.Windows.Forms.FormWindowState
+        $InitialFormWindowState = $MainForum.WindowState
+        #Init the OnLoad event to correct the initial state of the form
+        $MainForum.add_Load($OnLoadForm_StateCorrection)
+        #Show the Form
+    #endregion
+
     #region Add buttons to the form
-    function BuildButtons($buttonRows, $MainForm)
-    {
         $row = 0
         $col = 0
         $top = $buttonRows.top
@@ -90,12 +105,16 @@
                 $col++
                 $left+=($width + $buttonRows.margin)
 
-                $MainForm.Controls.Add($btn.ButtonObject)
+                $MainForum.Controls.Add($btn.ButtonObject)
             }
             $top+=($buttonRows.defaultHeight + $buttonRows.margin)
             $row++
         }
-    }
+
+    #endregion
+
+    #region Add elements to the Form
+        $MainForum.Controls.Add($richTextBox1)
 
     #endregion
 
@@ -103,37 +122,3 @@
         $MainForum.ShowDialog()| Out-Null
 
     #endregion
-
-
-
-function BuildButtonRows(){
-    $jsonFile = "c:\git\powershell\utilities\AzureDemoClipboard.json"
-    $buttonRows1 = ConvertFrom-Json -InputObject (Get-Content $jsonFile -Raw)
-    #Write-Output $buttonRows1
-    return $buttonRows1
-}
-
-$buttonRows2 = BuildButtonRows
-#Write-Output $buttonRows2
-BuildButtons $buttonRows2 $MainForum
-
-
-
-    #region Main form
-        $MainForum = New-Object System.Windows.Forms.Form
-        $System_Drawing_Size = New-Object System.Drawing.Size
-        $System_Drawing_Size.Height = 800
-        $System_Drawing_Size.Width = 800
-        $MainForum.ClientSize = $System_Drawing_Size
-        $MainForum.DataBindings.DefaultDataSourceUpdateMode = 0
-        $MainForum.Name = "MainForum"
-        $MainForum.Text = "Clipboarder"
-
-        #Save the initial state of the form
-        $InitialFormWindowState = New-Object System.Windows.Forms.FormWindowState
-        $InitialFormWindowState = $MainForum.WindowState
-        #Init the OnLoad event to correct the initial state of the form
-        $MainForum.add_Load($OnLoadForm_StateCorrection)
-        #Show the Form
-    #endregion
-
